9728e2321de79b939c3bb3d7453b8ae34935c53d
Aldrin Caballero <caballeroaldrin02@gmail.com>
2025-06-28 08:15:53 +0800

    Updated

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -8,8 +8,7 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('feed') }}" class="flex items-center space-x-2">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                        <span
-                            class="text-xl font-bold text-gray-800 dark:text-gray-200 hidden sm:block">SocialConnect</span>
+                        <span class="text-xl font-bold text-gray-800 dark:text-gray-200">SocialConnect</span>
                     </a>
                 </div>
 
