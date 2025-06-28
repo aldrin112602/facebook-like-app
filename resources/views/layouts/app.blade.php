29494a45192cb4f5cebfec8da7629e1057566327
Aldrin Caballero <caballeroaldrin02@gmail.com>
2025-06-28 08:13:40 +0800

    Remove search and notif

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
 
         <!-- Page Heading -->
         @isset($header)
-            <header class="fixed w-full top-0 left-0 dark:bg-gray-800 bg-white shadow" style="margin-top: 4rem;">
+            <header class="fixed w-full top-0 left-0 dark:bg-gray-800 bg-white shadow z-40" style="margin-top: 4rem;">
                 <div class="mx-auto py-6 px-4 sm:px-6 lg:px-8 max-w-7xl">
                     {{ $header }}
                 </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -54,30 +54,8 @@
                 </div>
             </div>
 
-            <!-- Right Side: Search and User Menu -->
+            <!-- Right Side: User Menu -->
             <div class="hidden sm:flex sm:items-center sm:space-x-4 sm:ms-6">
-                <!-- Search Bar (optional) -->
-                <div class="relative">
-                    <input type="text" placeholder="Search..."
-                        class="bg-gray-100 dark:bg-gray-700 rounded-full px-4 py-2 pl-10 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:text-gray-200">
-                    <svg class="w-4 h-4 absolute left-3 top-3 text-gray-400" fill="none" stroke="currentColor"
-                        viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                    </svg>
-                </div>
-
-                <!-- Notifications -->
-                <button
-                    class="relative p-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
-                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9">
-                        </path>
-                    </svg>
-                    <!-- Notification dot -->
-                    <span class="absolute top-0 right-0 block h-2 w-2 rounded-full bg-red-400"></span>
-                </button>
 
                 <!-- User Dropdown -->
                 <x-dropdown align="right" width="48">
